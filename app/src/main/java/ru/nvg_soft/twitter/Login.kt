ee177888363fdbc2690d49f145e6af6fa8a21fa8
DaniilShcherbakov <Charel@bk.ru>
2019-11-24 16:16:19 +0300

    Design login layout

---
+++ app/src/main/java/ru/nvg_soft/twitter/Login.kt
@@ -0,0 +1,20 @@
+package ru.nvg_soft.twitter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        ivImagePerson.setOnClickListener(View.OnClickListener {
+           // TODO: select image from phone
+        })
+    }
+
+    fun btnLoginEvent(view:View){}
+}
